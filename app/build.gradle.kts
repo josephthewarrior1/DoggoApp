1db28e23ed8bfb9bf09238f1474bce6fda7061bd
joseph imanuel setiawan <128193636+josephthewarrior1@users.noreply.github.com>
2025-10-13 20:12:52 +0700

    ubah dikit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.cardview:cardview:1.0.0")
     implementation("com.google.firebase:firebase-auth:23.1.0")
     implementation("com.google.firebase:firebase-database:21.0.0")
     testImplementation(libs.junit)

ba58b27b9e4c5bee4683493317bfeb83e8248e3f
Aldi Putra <aldiputradjojo64@gmail.com>
2025-10-13 10:53:24 +0700

    update

--- app/src/main/java/com/example/doggo/IntroActivity.kt
+++ app/src/main/java/com/example/doggo/IntroActivity.kt
@@ -2,20 +2,21 @@ package com.example.doggo
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.Button
+import com.example.doggo.R
+import com.example.doggo.SignInActivity
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
 
-        val btnLetsGo = findViewById<Button>(R.id.btnLetsGo)
-        btnLetsGo.setOnClickListener {
-            // ⬇️ Ubah MainActivity jadi SignUpActivity
-            val intent = Intent(this, SignInActivity::class.java)
-            startActivity(intent)
-            finish() // Supaya ga bisa balik ke intro
-        }
+        // Delay for 2.5 seconds before navigating to SignInActivity
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, SignInActivity::class.java))
+            finish()
+        }, 2500)
     }
 }
